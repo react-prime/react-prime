bca8514a3489998bb2b36c9dbc1d7d9d130ab654
Jorn Bostelaar <bostelaarj@gmail.com>
2017-02-15 19:10:31 +0100

    Added npm-run-all with new npm script

--- src/server/helpers/renderFullPage.js
+++ src/server/helpers/renderFullPage.js
@@ -1,19 +1,14 @@
 const renderFullPage = (html) => (
-	`<!doctype html>
-	<html>
-		<head>
-			<meta charset="utf-8">
-			<title>App</title>
+	`
+		<meta charset="utf-8">
+		<title>React Redux Boilerplate</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 
-			<link rel="stylesheet" type="text/css" href="style.css">
-			<meta name="apple-mobile-web-app-capable" content="yes">
-			<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-		</head>
-		<body>
-			<div id="app">${html}</div>
-			<script src="bundle.js"></script>
-		</body>
-	</html>`
+		<div id="app">${html}</div>
+		<script src="bundle.js"></script>
+	`
 );
 
 export default renderFullPage;

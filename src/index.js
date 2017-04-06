ed0e91d98d308042a38d5730278cd7c512ad1dc8
Jorn Bostelaar <bostelaarj@gmail.com>
2017-04-06 17:53:57 +0200

    Improved index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from 'app/components/Root';
 
-const APP_NODE = document.getElementById('app');
-
-ReactDOM.render((
-    <Root />
-), APP_NODE);
+const render = () => {
+    const Root = require('app/components/Root').default;
+    ReactDOM.render((
+        <Root />
+    ), document.getElementById('app'));
+};
 
 if (__DEV__ && module.hot) {
-    module.hot.accept('./app/components/Root', () => {
-        const NextRoot = require('./app/components/Root').default;
-        ReactDOM.render((
-            <NextRoot />
-        ), APP_NODE);
-    });
+    module.hot.accept('./app/components/Root', render);
 }
+
+render();

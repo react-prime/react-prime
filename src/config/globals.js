2e6f6cdfb06b7b0a0639739cd8e433b70cc57785
Jorn Bostelaar <bostelaarj@gmail.com>
2017-07-19 15:10:05 +0200

    Fixed custom PORT from env for production

--- src/config/globals.js
+++ src/config/globals.js
@@ -3,6 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 module.exports = type => ({
     'process.env': {
         NODE_ENV: JSON.stringify(env),
+        PORT: process.env.PORT || 3000,
     },
     __DEV__: env === 'development',
     __PROD__: env === 'production',

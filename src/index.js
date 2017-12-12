cdd2b5d7189f919ad0d711d6c11ecca442cdec05
Jorn Bostelaar <bostelaarj@gmail.com>
2017-12-12 17:47:53 +0100

    Fixed toggle between ReactDOM hydrate and render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /* eslint 'global-require': 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SSR } from 'config';
 
 const render = () => {
     const Root = require('app/components/Root').default;
-    ReactDOM.hydrate(<Root />, document.getElementById('app'));
+    const node = document.getElementById('app');
+
+    if (SSR && !__DEV__) {
+        ReactDOM.hydrate(<Root />, node);
+    } else {
+        ReactDOM.render(<Root />, node);
+    }
 };
 
 if (__DEV__ && module.hot) {

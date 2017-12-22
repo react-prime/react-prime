dbbe5f7f943146391ee1d7f400d9e5ab43f05a69
Jorn Bostelaar <bostelaarj@gmail.com>
2017-12-23 00:42:39 +0100

    Add styled-components ThemeProvider to Root component

--- src/app/components/Root.jsx
+++ src/app/components/Root.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import store from 'app/store';
+import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import theme from 'styles/theme';
 import 'app/static/favicon.ico';
 import App from './App';
 
 const Root = () => (
     <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
     </Provider>
 );
 
---
+++ src/app/styles/theme.js
@@ -0,0 +1,4 @@
+export default {
+    blue: '#3498db',
+    white: '#ffffff',
+};

d8cc5d461de2703b45645bed6915e4b2c246947d
Jorn Bostelaar <bostelaarj@gmail.com>
2017-12-23 00:45:56 +0100

    Add global styling to Root component

--- src/app/components/Root.jsx
+++ src/app/components/Root.jsx
@@ -3,9 +3,12 @@ import store from 'app/store';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import globalStyling from 'styles';
 import theme from 'styles/theme';
 import 'app/static/favicon.ico';
 import App from './App';
+
+globalStyling();
 
 const Root = () => (
     <Provider store={store}>
---
+++ src/app/styles/index.js
@@ -0,0 +1,10 @@
+import { injectGlobal } from 'styled-components';
+
+export default () => injectGlobal`
+    body {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: sans-serif;
+    }
+`;

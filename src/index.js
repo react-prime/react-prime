23ee745ca050a99593cf39360e74a60066fccd55
Ronny <ronny.rook@labela.nl>
2018-10-05 10:59:32 +0200

    Remove references to SSR

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 /* eslint 'global-require': 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { SSR } from 'config';
 
 const render = () => {
   const Root = require('app/components/Root').default;
-  ReactDOM[SSR && !__DEV__ ? 'hydrate' : 'render'](<Root />, document.getElementById('app'));
+  ReactDOM.render(<Root />, document.getElementById('app'));
 };
 
 if (__DEV__ && module.hot) {
--- src/server/index.js
+++ src/server/index.js
@@ -1,25 +1,16 @@
-import React from 'react';
 import express from 'express';
 import path from 'path';
-import { renderToString } from 'react-dom/server';
-import { ServerStyleSheet } from 'styled-components';
 import compress from 'compression';
-import { port, SSR } from 'config';
-import renderFullPage from 'server/helpers/renderFullPage';
-import ServerRoot from 'server/helpers/ServerRoot';
+import { port } from 'config';
+import fs from 'fs';
 
 const app = express();
 app.use(compress());
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 
-const sheet = new ServerStyleSheet();
 app.use((req, res) => {
-  const html = SSR
-    ? renderToString(<ServerRoot location={req.url} sheet={sheet.instance} />)
-    : ' ';
-
-  const styleTags = sheet.getStyleTags();
-  res.status(200).send(renderFullPage({ html, styleTags }));
+  const htmlBuffer = fs.readFileSync(path.join(__dirname, '../../dist', 'index.html'));
+  res.send(htmlBuffer.toString());
 });
 
 app.listen(port, () => {

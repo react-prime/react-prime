fe62b4a9f4750fd5f9f0e12ab23cbd43b1280b2e
Ronny <ronny.rook@labela.nl>
2018-10-05 11:49:37 +0200

    Serve index.html on production

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,13 @@ import express from 'express';
 import path from 'path';
 import compress from 'compression';
 import { port } from 'config';
-import fs from 'fs';
 
 const app = express();
 app.use(compress());
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 
 app.use((req, res) => {
-  const htmlBuffer = fs.readFileSync(path.join(__dirname, '../../dist', 'index.html'));
-  res.send(htmlBuffer.toString());
+  res.sendFile('/index.html');
 });
 
 app.listen(port, () => {

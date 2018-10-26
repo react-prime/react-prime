82656de2888e7601601a6047fe05435599c6e041
Ronny <ronny.rook@labela.nl>
2018-10-26 13:58:06 +0200

    Add Anchor routing component

--- src/app/components/modules/Demo/index.jsx
+++ src/app/components/modules/Demo/index.jsx
@@ -1,46 +1,20 @@
 import React from 'react';
-import PT from 'prop-types';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
 
-import { install } from 'ducks/demo';
-import LogoIcon from 'vectors/logo.svg';
-import Button from 'common/Button';
-import InstallationPassed from './components/InstallationPassed';
+import Anchor from 'common/Anchor';
 
 const CenteredSection = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 0 20px;
-
-  svg {
-    margin: 50px 0;
-  }
 `;
 
-
-const Demo = ({ installation: { passed, loading }, ...props }) => (
+const Demo = () => (
   <CenteredSection>
-    <LogoIcon />
-    {passed ? (
-      <InstallationPassed />
-    ) : (
-      <Button onClick={props.install}>
-        {loading ? 'Installing ...' : 'Test installation'}
-      </Button>
-    )}
+    React Prime
+    <Anchor to="/">Go to home page</Anchor>
   </CenteredSection>
 );
 
-Demo.propTypes = {
-  install: PT.func.isRequired,
-  installation: PT.shape({
-    loading: PT.bool,
-    passed: PT.bool,
-  }),
-};
-
-export default connect((state) => ({
-  installation: state.demo,
-}), { install })(Demo);
+export default Demo;

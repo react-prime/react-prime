e74683429603138f78aa107fdaa4f3d999febd25
Ronny <ronny.rook@labela.nl>
2018-10-26 13:18:02 +0200

    Update to Styled Components 4

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { lazy, Suspense } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import GlobalStyle from 'styles';
 
 import Loading from 'common/Loading';
 
@@ -7,14 +8,17 @@ const Prime = lazy(() => import('modules/Prime'));
 const Demo = lazy(() => import('modules/Demo'));
 
 const App = () => (
-  <main>
-    <Suspense fallback={<Loading>Loading...</Loading>}>
-      <Switch>
-        <Route path="/" component={Prime} exact />
-        <Route path="/demo" component={Demo} />
-      </Switch>
-    </Suspense>
-  </main>
+  <>
+    <main>
+      <GlobalStyle />
+      <Suspense fallback={<Loading>Loading...</Loading>}>
+        <Switch>
+          <Route path="/" component={Prime} exact />
+          <Route path="/demo" component={Demo} />
+        </Switch>
+      </Suspense>
+    </main>
+  </>
 );
 
-export default App;
+export default withRouter(App);
--- src/app/components/Root.jsx
+++ src/app/components/Root.jsx
@@ -3,12 +3,9 @@ import store from 'app/store';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import globalStyling from 'styles';
 import theme from 'styles/theme';
 import 'app/static/favicon.ico';
 import App from './App';
-
-globalStyling();
 
 const Root = () => (
   <Provider store={store}>

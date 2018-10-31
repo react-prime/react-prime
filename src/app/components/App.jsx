184d7294cfdf7f22914fc9d7779127ac7e1fa0a9
Ronny <ronny.rook@labela.nl>
2018-10-31 14:46:22 +0100

    Remove Fragment in App.jsx

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -8,17 +8,15 @@ const Prime = lazy(() => import('modules/Prime'));
 const Demo = lazy(() => import('modules/Demo'));
 
 const App = () => (
-  <>
-    <main>
-      <GlobalStyle />
-      <Suspense fallback={<Loading>Loading...</Loading>}>
-        <Switch>
-          <Route path="/" component={Prime} exact />
-          <Route path="/demo" component={Demo} />
-        </Switch>
-      </Suspense>
-    </main>
-  </>
+  <main>
+    <GlobalStyle />
+    <Suspense fallback={<Loading>Loading...</Loading>}>
+      <Switch>
+        <Route path="/" component={Prime} exact />
+        <Route path="/demo" component={Demo} />
+      </Switch>
+    </Suspense>
+  </main>
 );
 
 export default withRouter(App);

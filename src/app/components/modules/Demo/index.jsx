b77bc56e7efc67b3979201e0c1a59e1d53f8bff3
Ronny <ronny.rook@labela.nl>
2018-10-31 13:31:17 +0100

    Change text of Demo page

--- src/app/components/modules/Demo/index.jsx
+++ src/app/components/modules/Demo/index.jsx
@@ -7,7 +7,7 @@ import CenteredSection from 'common/CenteredSection';
 const Demo = () => (
   <CenteredSection variant="green">
     <LogoIcon />
-    <p>Created by <a href="https://github.com/JBostelaar/react-prime" target="_blank" rel="noopener noreferrer">@JBostelaar</a>, maintained with <a href="https://github.com/JBostelaar/react-prime/graphs/contributors" target="_blank" rel="noopener noreferrer">contributors</a> and at <a href="https://labela.nl/" target="_blank" rel="noopener noreferrer">LabelA</a>.</p>
+    <p>Created by <a href="https://github.com/JBostelaar/react-prime" target="_blank" rel="noopener noreferrer">@JBostelaar</a>, maintained by <a href="https://labela.nl/" target="_blank" rel="noopener noreferrer">LabelA</a> with <a href="https://github.com/JBostelaar/react-prime/graphs/contributors" target="_blank" rel="noopener noreferrer">contributors</a>.</p>
     <Anchor to="/" variant="green">Go to home page</Anchor>
   </CenteredSection>
 );

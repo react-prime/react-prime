dce0698590fa582fc65f02f77d35a23cc4cec871
Ronny <ronny.rook@labela.nl>
2018-11-09 13:52:04 +0100

    Only use Service Worker in production

--- src/index.js
+++ src/index.js
@@ -2,22 +2,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Register service worker
-if (!__DEV__) {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js')
-        .then((registration) => {
-          if (!__PROD__) {
-            console.info('SW registered: ', registration);
-          }
-        }).catch((error) => {
-          if (!__PROD__) {
-            console.error('SW registration failed: ', error);
-          }
-        });
-    });
-  }
+if ('serviceWorker' in navigator && !__DEV__) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then((registration) => {
+        if (!__PROD__) {
+          console.info(`Service Worker registered! Scope: ${registration.scope}`);
+        }
+      }).catch((err) => {
+        if (!__PROD__) {
+          console.error(`Service Worker registration failed: ${err}`);
+        }
+      });
+  });
 }
 
 const render = () => {

98ad5a93d4e412a3c147814ac17820f263222463
Ronny <ronny.rook@labela.nl>
2018-11-13 13:22:42 +0100

    Re-use root url as const

--- src/server/index.js
+++ src/server/index.js
@@ -7,12 +7,14 @@ const app = express();
 app.use(compress());
 app.use(express.static(path.resolve(__dirname, '../../dist')));
 
+const root = path.join(__dirname, '../../dist');
+
 app.use('/sw.js', (req, res) => {
-  res.sendFile('sw.js', { root: path.join(__dirname, '../../dist') });
+  res.sendFile('sw.js', { root });
 });
 
 app.use('*', (req, res) => {
-  res.sendFile('index.html', { root: path.join(__dirname, '../../dist') });
+  res.sendFile('index.html', { root });
 });
 
 app.listen(port, () => {

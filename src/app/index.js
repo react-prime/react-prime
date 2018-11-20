abd21815763ed4df1f5ae8bb7eda4c10f7c4fbe3
Jorn Bostelaar <bostelaarj@gmail.com>
2018-11-20 11:08:29 +0100

    Move register service worker function te separate file

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,9 @@
 /* eslint 'global-require': 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import registerServiceWorker from 'services/registerServiceWorker';
 
-if ('serviceWorker' in navigator && !__DEV__) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then((registration) => {
-        if (!__PROD__) {
-          console.info(`Service Worker registered! Scope: ${registration.scope}`);
-        }
-      }).catch((err) => {
-        if (!__PROD__) {
-          console.error(`Service Worker registration failed: ${err}`);
-        }
-      });
-  });
-}
+if (!__DEV__) registerServiceWorker();
 
 const render = () => {
   const Root = require('app/components/Root').default;

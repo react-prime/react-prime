3874ea3f295bd4b903125939b7522430063ca698
Jorn Bostelaar <bostelaarj@gmail.com>
2018-11-20 17:07:52 +0100

    Add new branding and remove components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,14 @@ import React, { lazy, Suspense } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import GlobalStyle from 'styles';
 
-import Loading from 'common/Loading';
-const Prime = lazy(() => import(/* webpackChunkName: "Prime" */'modules/Prime'));
-const Demo = lazy(() => import(/* webpackChunkName: "Demo" */'modules/Demo'));
+const Prime = lazy(() => import('modules/Prime'));
 
 const App = () => (
   <main>
     <GlobalStyle />
-    <Suspense fallback={<Loading>Loading...</Loading>}>
+    <Suspense fallback={<span>loading</span>}>
       <Switch>
         <Route path="/" component={Prime} exact />
-        <Route path="/demo" component={Demo} />
       </Switch>
     </Suspense>
   </main>

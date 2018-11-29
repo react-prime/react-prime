c00a4a946352e0f54fec7fffdcd6e45342cd97fd
Jorn Bostelaar <bostelaarj@gmail.com>
2018-11-29 13:23:20 +0100

    Fix deprecation warning of styled-components for attrs

--- src/components/common/Anchor/index.jsx
+++ src/components/common/Anchor/index.jsx
@@ -1,11 +1,11 @@
 import PT from 'prop-types';
 import styled from 'styled-components';
 
-const Anchor = styled.a.attrs({
+const Anchor = styled.a.attrs(({ href }) => ({
   target: '_blank',
   rel: 'noopener noreferrer',
-  href: (props) => props.href,
-})`
+  href,
+}))`
   color: ${(props) => props.theme.black};
   text-decoration: none;
   border-bottom: 2px solid ${(props) => props.theme.prime};

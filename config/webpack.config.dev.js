ceb0114961b404b37d96718eafbe6632bde5045d
Sander Vispoel <sander.vispoel@labela.nl>
2019-04-10 10:48:56 +0200

    Allow all addresses to connect to dev server

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -14,8 +14,9 @@ const devConfig = {
     new webpack.DefinePlugin(globals),
   ],
   devServer: {
-    hot: true,
+    host: '0.0.0.0',
     port: process.env.PORT || 3000,
+    hot: true,
     stats: 'minimal',
     historyApiFallback: true,
   },

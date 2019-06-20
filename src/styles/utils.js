392820ba2f6a495374dddea29ed0aabe8b4c63cc
Jorn Bostelaar <bostelaarj@gmail.com>
2019-06-20 15:59:15 +0200

    Fix style linting errors

--- src/styles/utils.js
+++ src/styles/utils.js
@@ -10,7 +10,7 @@ export const media = Object.keys(sizes).reduce((accumulator, label) => {
   // use em in breakpoints to work properly cross-browser and support users
   // changing their browsers font-size: https://zellwk.com/blog/media-query-units/
   const emSize = sizes[label] / 16;
-  accumulator[label] = (...args) => css`@media (min-width: ${emSize}em) {${css(...args)};}`;
+  accumulator[label] = (...args) => css` @media (min-width: ${emSize}em) { ${css(...args)}; } `;
   return accumulator;
 }, {});
 

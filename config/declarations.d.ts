7e4c59b0f2ef93a57b88402280dce36d10241f25
Sander Vispoel <sander.vispoel@labela.nl>
2019-07-26 14:50:14 +0200

    Add more image file declarations & add font file declarations

--- config/declarations.d.ts
+++ config/declarations.d.ts
@@ -16,15 +16,75 @@ declare interface NodeModule {
   };
 }
 
-// files
-declare module '*.svg';
-declare module '*.gif';
+// We have to declare how files other than .ts(x) or .js(x) are handled by our codebase because
+// Typescript does not know we are handling these files with Webpack.
+declare module '*.json' {
+  const value: object;
+  export default value;
+}
+
+// Image files
+declare module '*.svg' {
+  const value: string;
+  export = value;
+}
+declare module '*.gif' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpg' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpeg' {
+  const value: string;
+  export = value;
+}
 declare module '*.png' {
   const value: string;
   export = value;
 }
-declare module '*.json' {
-  const value: any;
-  export default value;
+
+// Image files external import
+declare module '*.svg?external' {
+  const value: string;
+  export = value;
 }
-declare module '*.svg?external';
+declare module '*.gif?external' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpg?external' {
+  const value: string;
+  export = value;
+}
+declare module '*.jpeg?external' {
+  const value: string;
+  export = value;
+}
+declare module '*.png?external' {
+  const value: string;
+  export = value;
+}
+
+// Font files
+declare module '*.otf' {
+  const value: string;
+  export = value;
+}
+declare module '*.ttf' {
+  const value: string;
+  export = value;
+}
+declare module '*.woff' {
+  const value: string;
+  export = value;
+}
+declare module '*.woff2' {
+  const value: string;
+  export = value;
+}
+declare module '*.eot' {
+  const value: string;
+  export = value;
+}

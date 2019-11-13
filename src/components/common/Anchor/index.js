4325e44d447dfcae6b152dd6127c648e3b08ea58
Jorn Bostelaar <bostelaarj@gmail.com>
2019-11-13 11:49:16 +0100

    Remove unnecessary href attribute in Anchor component

--- src/components/common/Anchor/index.js
+++ src/components/common/Anchor/index.js
@@ -1,18 +1,12 @@
-import PT from 'prop-types';
 import styled from 'styled-components';
 
-const Anchor = styled.a.attrs(({ href }) => ({
+const Anchor = styled.a.attrs(() => ({
   target: '_blank',
   rel: 'noopener noreferrer',
-  href,
 }))`
   color: ${(props) => props.theme.black};
   text-decoration: none;
   border-bottom: 2px solid ${(props) => props.theme.prime};
 `;
-
-Anchor.propTypes = {
-  href: PT.string,
-};
 
 export default Anchor;

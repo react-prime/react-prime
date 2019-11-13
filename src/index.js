15817e6e2b09357ce6d7498784fa5be5e314b8fc
Jorn Bostelaar <bostelaarj@gmail.com>
2019-11-13 13:06:48 +0100

    Add new line to src/index.js for imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint 'global-require': 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import registerServiceWorker from 'services/registerServiceWorker';
 
 if (!__DEV__) registerServiceWorker();

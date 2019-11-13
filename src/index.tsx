c5e13289aec4553a8a97a6d3eb8ad095557bc0f3
Jorn Bostelaar <bostelaarj@gmail.com>
2019-11-13 15:27:06 +0100

    Improve HMR in src/index

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import registerServiceWorker from 'services/registerServiceWorker';
 
+import Root from './components/Root';
+
 if (!__DEV__) registerServiceWorker();
+if (__DEV__ && module.hot) module.hot.accept();
 
-const render = () => {
-  const Root = require('./components/Root').default;
-  ReactDOM.render(<Root />, document.getElementById('app'));
-};
+ReactDOM.render(<Root />, document.getElementById('app'));
 
-if (__DEV__ && module.hot) {
-  module.hot.accept('./components/Root', render);
-}
-
-render();

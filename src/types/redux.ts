5d7bab7e0913752036928cf30144e7057668f8ff
sandervspl <sandervispoel@hotmail.com>
2021-04-19 17:21:37 +0200

    Omit $CombinedState from store type

--- src/types/redux.ts
+++ src/types/redux.ts
@@ -1,5 +1,5 @@
 import store from 'store';
 
-export type Store = ReturnType<typeof store.getState>;
+export type Store = Omit<ReturnType<typeof store.getState>, '$CombinedState'>;
 
 // All custom typing removed!

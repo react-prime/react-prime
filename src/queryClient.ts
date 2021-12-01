68d9e1c988f08e48f1e57d1cdc8e1d54d3b1c87a
Ronny Rook <ronny@labela.nl>
2021-12-01 11:47:59 +0100

    Change queryClient staleTime value

--- src/queryClient.ts
+++ src/queryClient.ts
@@ -3,7 +3,7 @@ import { QueryClient } from 'react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 30000,
+      staleTime: 30 * 1000, // 30 seconds
       cacheTime: 1000 * 6 * 10, // 10 minutes
       retry: false,
     },

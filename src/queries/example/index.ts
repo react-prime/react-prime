a2757f16ff27f1807166bf452007aaa11581a713
Ronny Rook <ronny@labela.nl>
2021-12-01 11:58:13 +0100

    Remove fetcher and add Promise examples in queries

--- src/queries/example/index.ts
+++ src/queries/example/index.ts
@@ -1,21 +1,20 @@
 import * as i from 'types';
 import { useMutation, useQueryClient, useQuery, UseQueryResult } from 'react-query';
 
-import { fetcher } from 'services';
-
 export const useCreateUser = () => {
   const queryClient = useQueryClient();
 
   return useMutation(
-    async (data: i.DataPayload) => await fetcher(
-      '/create-user',
-      {
-        method: 'POST',
-        body: JSON.stringify({ data }),
-      },
-    ),
+    async (data: i.DataPayload) => await new Promise<i.Data>((resolve) => {
+      setTimeout(() => {
+        resolve({
+          id: '3783ce59-0e59-4a77-aaaf-e824f7c5e8f1',
+          ...data,
+        });
+      }, 1000);
+    }),
     {
-      onSuccess: ({ data }: { data: i.Data }) => {
+      onSuccess: (data: i.Data) => {
         queryClient.invalidateQueries(['user', data.id]);
         queryClient.setQueryData(['user', data.id], data);
       },
@@ -26,11 +25,14 @@ export const useCreateUser = () => {
 export const useGetUser = (userId: string): UseQueryResult<i.Data | undefined> => {
   return useQuery(
     ['entry', userId],
-    async () => await fetcher(
-      '/get-user',
-      { method: 'GET' },
-      { id: userId },
-    ),
+    async () => await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          id: '3783ce59-0e59-4a77-aaaf-e824f7c5e8f1',
+          name: 'John Doe',
+        });
+      }, 1000);
+    }),
     {
       enabled: Boolean(userId),
     },
